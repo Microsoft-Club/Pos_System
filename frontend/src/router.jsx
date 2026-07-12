82d3b63a6eb48a891180093b4f57776b2f480ba5
Rayyan Arif <rayyanarif114@gmail.com>
2026-07-13 00:44:27 +0500

    react routing implemented and frontend structure finalized

---
+++ frontend/src/router.jsx
@@ -0,0 +1,11 @@
+import {Route, createBrowserRouter, createRouteFromElements} from 'react-router-dom';
+
+const router = createBrowserRouter(
+    createRouteFromElements(
+        <>
+            
+        </>
+    )
+);
+
+export default router;
